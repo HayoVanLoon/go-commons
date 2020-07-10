604283f9ff703072d535ee7ed3d819ca1b20d688
Hayo van Loon <hayovanloon@gmail.com>
2020-07-10 13:43:28 +0200

    update notice

--- logjson/logjson.go
+++ logjson/logjson.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Hayo van Loon
+ * Copyright 2020 Hayo van Loon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
